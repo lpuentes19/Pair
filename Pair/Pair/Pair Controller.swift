e968e433cc07ba4a214bfe184ef7eeb7b3afadfb
lpuentes19 <luey@Luiss-MacBook-Pro.local>
2017-06-02 10:09:17 -0600

    Completed Model Controller

--- Pair/Pair/Pair Controller.swift
+++ Pair/Pair/Pair Controller.swift
@@ -7,8 +7,58 @@
 //
 
 import Foundation
+import CloudKit
 
 class PairController {
     
+    static let shared = PairController()
     
+    var names = [Pair]()
+    
+    // Save names to CloudKit
+    func addName(name: String) {
+        
+        let name = Pair(names: name)
+        
+        let nameRecord = name.cloudKitRecord
+        
+        CKContainer.default().publicCloudDatabase.save(nameRecord) { (record, error) in
+            if let error = error {
+                NSLog("Error saving name to CloudKit: \(error)")
+            }
+            self.names.append(name)
+        }
+    }
+    
+    // Fetch from CloudKit
+    func fetchName() {
+        
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: Pair.typeKey, predicate: predicate)
+        
+        CKContainer.default().publicCloudDatabase.perform(query, inZoneWith: nil) { (record, error) in
+            if let error = error {
+                NSLog("Error fetching names from CloudKit: \(error)")
+            }
+            
+            guard let record = record else { return }
+            
+            let names = record.flatMap { Pair(cloudKitRecord: $0) }
+            self.names = names
+        }
+    }
+    
+    // Delete from CloudKit
+    
+    func delete(names: Pair) {
+        
+        guard let index = names.index(of: names) else { return }
+        self.names.remove(at: index)
+        
+        CKContainer.default().publicCloudDatabase.delete(withRecordID: name.cloudKitRecord.recordID) { (record, error) in
+            if let error = error {
+                NSLog("Error deleting name from CloudKit: \(error)")
+            }
+        }
+    }
 }
